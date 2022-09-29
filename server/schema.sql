7eb5b061d85805396f9eab275c540d6646947996
nhasty <nyall.hasty@gmail.com>
2022-09-28 17:33:02 -0700

    work on shema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,36 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER AUTO_INCREMENT,
+  message_text VARCHAR(140) NOT NULL,
+  users_id INTEGER,
+  rooms_id INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (users_id) REFERENCES users(id),
+  FOREIGN KEY (rooms_id) REFERENCES rooms(id)
 );
 
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE friends (
+  id INTEGER AUTO_INCREMENT,
+  user1_id INTEGER,
+  user2_id INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user1_id) REFERENCES users(id),
+  FOREIGN KEY (user2_id) REFERENCES users(id)
+);
 /* Create other tables and define schemas for them here! */
 
-
+CREATE TABLE rooms (
+  id INTEGER AUTO_INCREMENT,
+  roomname VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
 
 
 /*  Execute this file from the command line by typing:
